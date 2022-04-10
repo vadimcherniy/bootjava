1661dfd5573bea074f1f68e0c6ec25d4ad75742c
vadym.chornyy <vchornyi@intelliarts.com>
2022-04-10 13:40:57 +0300

    tests update

--- src/test/java/ru/javaops/bootjava/BootJavaApplicationTests.java
+++ src/test/java/ru/javaops/bootjava/BootJavaApplicationTests.java
@@ -1,12 +1,10 @@
 package ru.javaops.bootjava;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BootJavaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 }
